a05bb7e30e1d1646fb901bcf22e331955d8ee94e
Jon Kracht <jonkracht@gmail.com>
2025-10-30 13:03:01 -0400

    Beginning figure creation scripting

--- pdga_score_scraper/plots.py
+++ pdga_score_scraper/plots.py
@@ -4,25 +4,28 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 from pdga_score_scraper.config import FIGURES_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = FIGURES_DIR / "plot.png",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating plot from data...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Plot generation complete.")
-    # -----------------------------------------
+def main():
+
+    # TODO:  eventually pull from argument
+    event_id = 89433
+
+    # Load processed data
+    df = pd.read_pickle(PROCESSED_DATA_DIR / f"{event_id}-processed.pkl")
+
+    logger.success(f"Loaded data for Event {event_id}")
+
+    logger.info("Beginning plot generation")
+
+    print(df.columns)
 
 
 if __name__ == "__main__":
